f7852296a530a591aff1f9c603fa6c3baffdbaef
asyscom <50611111+asyscom@users.noreply.github.com>
2019-05-14 09:19:31 +0200

    Update execute.php

--- execute.php
+++ execute.php
@@ -5,7 +5,8 @@ if(!$update)
 {
   exit;
 }
-$ricco = “https://www.meteospezia.com/rete/felettino.txt";
+//ricco = “https://www.meteospezia.com/rete/felettino.txt";
+$ricco = "file";
 $gestione = fopen($ricco, “w”);
 
 $message = isset($update['message']) ? $update['message'] : "";
