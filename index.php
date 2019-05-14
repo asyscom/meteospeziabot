a52071d5d64480ec7067c0d761a144b98316e604
asyscom <50611111+asyscom@users.noreply.github.com>
2019-05-14 08:27:57 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,20 @@
-<?php echo "Silence is golden";
+<?php
+
+  $botToken = "659309900:AAGL0fAz8iCwFpkD8SL-qvFrk7TIFzQzbjc";
+  $website = "https://api.telegram.org/bot".$botToken;
+  $updates = file_get_contents("php://input");
+  $updates = json_decode($update, TRUE);
+   
+  $chatID = $updates['message']['chat']['id'];
+  $text = $updates['message']['from']['username'];
+  
+  sendMessage($chatID, $text);
+  
+  function sendMessage($chatID,$message)
+  {
+  $url = $GLOBALS[website]."/sendMessage?chat_id=".$chatID."&text=".urlencode($message);
+  file_get_contents($url);
+  }
+  
+  
+?>
